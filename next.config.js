3ff70f4e9c76cb3e32b428e68f7e0c2345f1fab0
ttc0915 <ttc458303@gmail.com>
2025-07-16 20:52:08 +0800

    feat: upload all web-site files

--- next.config.js
+++ next.config.js
@@ -10,14 +10,17 @@ const nextConfig = {
       },
     ],
   },
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: '/api/:path*',
-  //       destination: 'http://127.0.0.1:8000/api/:path*',
-  //     },
-  //   ]
-  // },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://127.0.0.1:8000/api/:path*',
+      },
+    ]
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
--- src/app/api/status/[id]/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server';
-
-export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
-  const { id } = params;
-  const res = await fetch(`https://dreamina-worker.taylortang458303.workers.dev/api/status/${id}`);
-  const data = await res.json();
-  return NextResponse.json(data, { status: res.status });
-} 
